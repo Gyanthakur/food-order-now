f39ade6acae32f67f48aeabed56da77ba37292c8
Gyanthakur <gps.96169@gmail.com>
2024-10-14 17:17:39 +0530

    feat: added food navbar header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>Craving something delicious? Browse through our menu and order your favorite meals in just a few clicks. Fast delivery and mouth-watering dishes await you!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+import './Header.css'
+export default Header
---
+++ src/pages/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Cart.css'
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+import './Cart.css'
+export default Cart
